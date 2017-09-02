1a4f92f9693a078400c0b90b31ec9aca74b45aec
anjuvaos <valenciaosorioa@g.cofc.edu>
2017-09-02 23:24:49 +0000

    finished all 34 tests

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,82 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  sum=0
+  arr.each{|ele|sum+=ele}
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length==1
+  m=arr.sort{|a,b|b<=>a}
+  sum=m[0]+m[1]
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  for x in 0..arr.length-1
+    for y in 0..arr.length-1
+        if x!=y
+           return true if arr[x]+arr[y]==n
+        end     
+    end
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}".to_s
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  first=s.chr.downcase
+  return true if "bcdfghjklmnpqrstvwxyz".include? first
+  false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  s.each_char{|c| return false if c!='0' && c!='1'}
+  dec=0
+  pot=0
+  s.reverse!
+  s.each_char{|c|
+               dec=dec+(c.to_i*2**pot)
+               pot+=1
+  }
+  return true if dec % 4 == 0
+  false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
-end
+  def initialize isbn,price
+      raise ArgumentError if isbn.empty? || price <= 0
+      @isbn=isbn
+      @price=price
+  end
+  attr_accessor :isbn
+  attr_accessor :price
+  
+  def price_as_string
+    num=@price.to_s
+    long=num.length
+    if num.include? "."
+      x=num.index(".")
+      if x >long - 3 # this is a price with 1 decimal
+       num=num+"0" 
+      else # this is a price with more that 2 decimals
+       num=num.slice(0..x+2)
+      end   
+    else
+      num=num+".00" # this is a price without decimal 
+    end
+    return "$"+num
+  end
+end  
